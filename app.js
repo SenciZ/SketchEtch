1a4155d57b23a3d4514b04770f93784d84608374
SenciZ <zenkovic88@gmail.com>
2021-03-26 14:40:39 -0500

    Create dynamic grid with javacsript and create buttons to control etch area

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-let cell = document.querySelectorAll(".cell");
-for (let i = 0; i<256; i++){
-    cell[i].addEventListener("mouseenter", function () {
-            cell[i].classList.add("cellNew");
-    })
+const gridContainer = document.getElementById("gridContainer");
+
+let gridSize;
+
+createGrid();
+
+function createGrid(){
+    gridSize = Math.pow(prompt("Enter number between 4 and 64 for desired grid size"), 2);
+    for(let i = 0; i < gridSize; i++){
+        const cell = document.createElement("div")
+        cell.classList.add("cell");
+            cell.addEventListener("mouseenter", function(){
+            cell.classList.add("cellNew");
+            });
+        gridContainer.appendChild(cell);
+        gridContainer.style.gridTemplateColumns = `repeat(${Math.sqrt(gridSize)}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${Math.sqrt(gridSize)}, 1fr)`;
+    }
 }
+
+
+let createBtn = document.querySelector(".create");
+createBtn.addEventListener("click", function (){
+    while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid();
+});
+
 
 let clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", function (){
     clearGrid();
 });
 
+
 function clearGrid(){
-    for (let i = 0; i<256; i++){
-        cell[i].classList.remove("cellNew");
+    for (let i = 0; i < gridSize; i++){
+        const cells = document.querySelectorAll(".cell")
+        cells[i].classList.remove("cellNew");
     }
 }
 
